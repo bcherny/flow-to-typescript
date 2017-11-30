c329e65ac2f5696838804c301101251c9b29c72c
Boris Cherny <boris@performancejs.com>
2017-11-29 18:06:22 -0800

    clean up dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import generate from '@babel/generator'
+import generate from 'babel-generator'
 import traverse, { Node, Visitor } from 'babel-traverse'
 import { File } from 'babel-types'
 import { parse } from 'babylon'
