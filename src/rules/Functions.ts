9a4bcd4d17ee89340d4a49315ec9545af2e49961
Boris Cherny <boris@performancejs.com>
2017-12-19 19:56:07 -0800

    bugfixes

--- src/rules/Functions.ts
+++ src/rules/Functions.ts
@@ -1,23 +1,8 @@
-import { functionTypeAnnotation, functionTypeParam, identifier } from 'babel/packages/babel-types'
 import { addRule } from '../'
-import { generateFreeIdentifier } from '../utils'
+import { toTs } from '../convert'
 
 addRule('Functions', () => ({
   FunctionTypeAnnotation(path) {
-
-    if (path.node.params.every(_ => _.name !== null)) {
-      return
-    }
-
-    let paramNames = path.node.params.map(_ => _.name).filter(_ => _ !== null).map(_ => _.name)
-    let params = path.node.params.map(param => {
-      if (param.name === null) {
-        let id = generateFreeIdentifier(paramNames)
-        paramNames.push(id)
-        return functionTypeParam(identifier(id), param.typeAnnotation)
-      }
-      return param
-    })
-    path.replaceWith(functionTypeAnnotation(path.node.typeParameters, params, path.node.rest, path.node.returnType))
+    path.replaceWith(toTs(path.node))
   }
 }))
