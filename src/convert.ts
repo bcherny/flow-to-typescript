4f62e0d7ffac49d3077ccdf6bc7caa40883d5991
Benjie Gillam <benjie@jemjie.com>
2019-01-04 17:29:35 +0000

    Fixes

--- src/convert.ts
+++ src/convert.ts
@@ -1,215 +1,309 @@
-import { booleanLiteral, Flow, FlowType, FunctionTypeAnnotation, identifier, Identifier, isSpreadProperty, isTSTypeParameter, isTypeParameter, Node, numericLiteral, stringLiteral, tsAnyKeyword, tsArrayType, tsAsExpression, tsBooleanKeyword, tsFunctionType, TSFunctionType, tsIntersectionType, tsLiteralType, tsNullKeyword, tsNumberKeyword, tsPropertySignature, tsStringKeyword, tsThisType, tsTupleType, TSType, tsTypeAnnotation, tsTypeLiteral, tsTypeParameter, tsTypeParameterDeclaration, tsTypeQuery, tsTypeReference, tsUndefinedKeyword, tsUnionType, tsVoidKeyword, TypeAnnotation, TypeParameter } from '@babel/types'
-import { generateFreeIdentifier } from './utils'
+import {
+  booleanLiteral,
+  Flow,
+  FlowType,
+  FunctionTypeAnnotation,
+  identifier,
+  Identifier,
+  isSpreadProperty,
+  isTSTypeParameter,
+  isTypeParameter,
+  Node,
+  numericLiteral,
+  stringLiteral,
+  tsAnyKeyword,
+  tsArrayType,
+  tsAsExpression,
+  tsBooleanKeyword,
+  tsFunctionType,
+  TSFunctionType,
+  tsIntersectionType,
+  tsLiteralType,
+  tsNullKeyword,
+  tsNumberKeyword,
+  tsPropertySignature,
+  tsStringKeyword,
+  tsThisType,
+  tsTupleType,
+  TSType,
+  tsTypeAnnotation,
+  tsTypeLiteral,
+  tsTypeParameter,
+  tsTypeParameterDeclaration,
+  tsTypeQuery,
+  tsTypeReference,
+  tsUndefinedKeyword,
+  tsUnionType,
+  tsVoidKeyword,
+  TypeAnnotation,
+  TypeParameter,
+  tsQualifiedName,
+  QualifiedTypeIdentifier,
+} from "@babel/types";
+import { generateFreeIdentifier } from "./utils";
 
 // TODO: Add overloads
 export function toTs(node: Flow | TSType): TSType {
+  console.log(`\t ${node.type}`);
   switch (node.type) {
-
     // TS types
     // TODO: Why does tsTs get called with TSTypes? It should only get called with Flow types.
-    case 'TSAnyKeyword':
-    case 'TSArrayType':
-    case 'TSBooleanKeyword':
-    case 'TSConstructorType':
-    case 'TSExpressionWithTypeArguments':
-    case 'TSFunctionType':
-    case 'TSIndexedAccessType':
-    case 'TSIntersectionType':
-    case 'TSLiteralType':
-    case 'TSMappedType':
-    case 'TSNeverKeyword':
-    case 'TSNullKeyword':
-    case 'TSNumberKeyword':
-    case 'TSObjectKeyword':
-    case 'TSParenthesizedType':
-    case 'TSStringKeyword':
-    case 'TSSymbolKeyword':
-    case 'TSThisType':
-    case 'TSTupleType':
-    case 'TSTypeAnnotation':
-    case 'TSTypeLiteral':
-    case 'TSTypeOperator':
-    case 'TSTypePredicate':
-    case 'TSTypeQuery':
-    case 'TSTypeReference':
-    case 'TSUndefinedKeyword':
-    case 'TSUnionType':
-    case 'TSVoidKeyword':
-    case 'TSTypeParameterDeclaration':
-    case 'TSAsExpression':
-    case 'TSPropertySignature':
-      return node
+    case "TSAnyKeyword":
+    case "TSArrayType":
+    case "TSBooleanKeyword":
+    case "TSConstructorType":
+    case "TSExpressionWithTypeArguments":
+    case "TSFunctionType":
+    case "TSIndexedAccessType":
+    case "TSIntersectionType":
+    case "TSLiteralType":
+    case "TSMappedType":
+    case "TSNeverKeyword":
+    case "TSNullKeyword":
+    case "TSNumberKeyword":
+    case "TSObjectKeyword":
+    case "TSParenthesizedType":
+    case "TSStringKeyword":
+    case "TSSymbolKeyword":
+    case "TSThisType":
+    case "TSTupleType":
+    case "TSTypeAnnotation":
+    case "TSTypeLiteral":
+    case "TSTypeOperator":
+    case "TSTypePredicate":
+    case "TSTypeQuery":
+    case "TSTypeReference":
+    case "TSUndefinedKeyword":
+    case "TSUnionType":
+    case "TSVoidKeyword":
+    case "TSTypeParameterDeclaration":
+    case "TSAsExpression":
+    case "TSPropertySignature":
+      return node;
 
     // Flow types
-    case 'AnyTypeAnnotation':
-    case 'ArrayTypeAnnotation':
-    case 'BooleanTypeAnnotation':
-    case 'BooleanLiteralTypeAnnotation':
-    case 'FunctionTypeAnnotation':
-    case 'GenericTypeAnnotation':
-    case 'IntersectionTypeAnnotation':
-    case 'MixedTypeAnnotation':
-    case 'NullableTypeAnnotation':
-    case 'NullLiteralTypeAnnotation':
-    case 'NumericLiteralTypeAnnotation':
-    case 'NumberTypeAnnotation':
-    case 'StringLiteralTypeAnnotation':
-    case 'StringTypeAnnotation':
-    case 'ThisTypeAnnotation':
-    case 'TupleTypeAnnotation':
-    case 'TypeofTypeAnnotation':
-    case 'TypeAnnotation':
-    case 'ObjectTypeAnnotation':
-    case 'UnionTypeAnnotation':
-    case 'VoidTypeAnnotation':
-      return toTsType(node)
+    case "AnyTypeAnnotation":
+    case "ArrayTypeAnnotation":
+    case "BooleanTypeAnnotation":
+    case "BooleanLiteralTypeAnnotation":
+    case "FunctionTypeAnnotation":
+    case "GenericTypeAnnotation":
+    case "IntersectionTypeAnnotation":
+    case "MixedTypeAnnotation":
+    case "NullableTypeAnnotation":
+    case "NullLiteralTypeAnnotation":
+    case "NumericLiteralTypeAnnotation":
+    case "NumberTypeAnnotation":
+    case "StringLiteralTypeAnnotation":
+    case "StringTypeAnnotation":
+    case "ThisTypeAnnotation":
+    case "TupleTypeAnnotation":
+    case "TypeofTypeAnnotation":
+    case "TypeAnnotation":
+    case "ObjectTypeAnnotation":
+    case "UnionTypeAnnotation":
+    case "VoidTypeAnnotation":
+      return toTsType(node);
 
-    case 'ObjectTypeProperty':
-      let _ = tsPropertySignature(node.key, tsTypeAnnotation(toTs(node.value)))
-      _.optional = node.optional
-      _.readonly = node.variance && node.variance.kind === 'minus'
-      return _
+    case "ObjectTypeProperty":
+      let _ = tsPropertySignature(node.key, tsTypeAnnotation(toTs(node.value)));
+      _.optional = node.optional;
+      _.readonly = node.variance && node.variance.kind === "minus";
+      return _;
 
-    case 'TypeCastExpression':
-      return tsAsExpression(node.expression, toTs(node.typeAnnotation))
+    case "TypeCastExpression":
+      return tsAsExpression(node.expression, toTs(node.typeAnnotation));
 
-    case 'TypeParameterDeclaration':
+    case "TypeParameterDeclaration":
       let params = node.params.map(_ => {
-        let d = (_ as any as TypeParameter).default
+        let d = ((_ as any) as TypeParameter).default;
         let p = tsTypeParameter(
           hasBound(_) ? toTsType(_.bound.typeAnnotation) : undefined,
           d ? toTs(d) : undefined
-        )
-        p.name = _.name
-        return p
-      })
+        );
+        p.name = _.name;
+        return p;
+      });
 
-      return tsTypeParameterDeclaration(params)
+      return tsTypeParameterDeclaration(params);
 
-    case 'ClassImplements':
-    case 'ClassProperty':
-    case 'DeclareClass':
-    case 'DeclareFunction':
-    case 'DeclareInterface':
-    case 'DeclareModule':
-    case 'DeclareTypeAlias':
-    case 'DeclareVariable':
-    case 'ExistentialTypeParam':
-    case 'FunctionTypeParam':
-    case 'InterfaceExtends':
-    case 'InterfaceDeclaration':
-    case 'TypeAlias':
-    case 'TypeParameterInstantiation':
-    case 'ObjectTypeCallProperty':
-    case 'ObjectTypeIndexer':
-    case 'QualifiedTypeIdentifier':
-      throw 'wut'
+    case "ClassImplements":
+    case "ClassProperty":
+    case "DeclareClass":
+    case "DeclareFunction":
+    case "DeclareInterface":
+    case "DeclareModule":
+    case "DeclareTypeAlias":
+    case "DeclareVariable":
+    case "ExistentialTypeParam":
+    case "FunctionTypeParam":
+    case "InterfaceExtends":
+    case "InterfaceDeclaration":
+    case "TypeAlias":
+    case "TypeParameterInstantiation":
+    case "ObjectTypeCallProperty":
+    case "ObjectTypeIndexer":
+    case "QualifiedTypeIdentifier":
+      console.dir(node);
+      throw "wut";
   }
 }
 
 export function toTsType(node: FlowType): TSType {
   switch (node.type) {
-    case 'AnyTypeAnnotation': return tsAnyKeyword()
-    case 'ArrayTypeAnnotation': return tsArrayType(toTsType(node.elementType))
-    case 'BooleanTypeAnnotation': return tsBooleanKeyword()
-    case 'BooleanLiteralTypeAnnotation': return tsLiteralType(booleanLiteral(node.value!))
-    case 'FunctionTypeAnnotation': return functionToTsType(node)
-    case 'GenericTypeAnnotation': return tsTypeReference(node.id)
-    case 'IntersectionTypeAnnotation': return tsIntersectionType(node.types.map(toTsType))
-    case 'MixedTypeAnnotation': return tsAnyKeyword()
-    case 'NullLiteralTypeAnnotation': return tsNullKeyword()
-    case 'NullableTypeAnnotation': return tsUnionType([toTsType(node.typeAnnotation), tsNullKeyword(), tsUndefinedKeyword()])
-    case 'NumberLiteralTypeAnnotation': return tsLiteralType(numericLiteral(node.value!))
-    case 'NumberTypeAnnotation': return tsNumberKeyword()
-    case 'StringLiteralTypeAnnotation': return tsLiteralType(stringLiteral(node.value!))
-    case 'StringTypeAnnotation': return tsStringKeyword()
-    case 'ThisTypeAnnotation': return tsThisType()
-    case 'TupleTypeAnnotation': return tsTupleType(node.types.map(toTsType))
-    case 'TypeofTypeAnnotation': return tsTypeQuery(getId(node.argument))
-    case 'ObjectTypeAnnotation': return tsTypeLiteral([
-      ...node.properties.map(_ => {
-        if (isSpreadProperty(_)) {
-          return _
-        }
-        let s = tsPropertySignature(_.key, tsTypeAnnotation(toTsType(_.value)))
-        s.optional = _.optional
-        return s
-        // TODO: anonymous indexers
-        // TODO: named indexers
-        // TODO: call properties
-        // TODO: variance
-      })
-      // ...node.indexers.map(_ => tSIndexSignature())
-    ])
-    case 'UnionTypeAnnotation': return tsUnionType(node.types.map(toTsType))
-    case 'VoidTypeAnnotation': return tsVoidKeyword()
+    case "AnyTypeAnnotation":
+      return tsAnyKeyword();
+    case "ArrayTypeAnnotation":
+      return tsArrayType(toTsType(node.elementType));
+    case "BooleanTypeAnnotation":
+      return tsBooleanKeyword();
+    case "BooleanLiteralTypeAnnotation":
+      return tsLiteralType(booleanLiteral(node.value!));
+    case "FunctionTypeAnnotation":
+      return functionToTsType(node);
+    case "GenericTypeAnnotation":
+      if (node.id.type === "QualifiedTypeIdentifier") {
+        const qualifiedTypeIdentifier: QualifiedTypeIdentifier = node.id;
+        console.dir(node);
+        return tsTypeReference(
+          tsQualifiedName(
+            qualifiedTypeIdentifier.qualification,
+            qualifiedTypeIdentifier.id
+          )
+        );
+      } else {
+        return tsTypeReference(node.id);
+      }
+    case "IntersectionTypeAnnotation":
+      return tsIntersectionType(node.types.map(toTsType));
+    case "MixedTypeAnnotation":
+      return tsAnyKeyword();
+    case "NullLiteralTypeAnnotation":
+      return tsNullKeyword();
+    case "NullableTypeAnnotation":
+      return tsUnionType([
+        toTsType(node.typeAnnotation),
+        tsNullKeyword(),
+        tsUndefinedKeyword(),
+      ]);
+    case "NumberLiteralTypeAnnotation":
+      return tsLiteralType(numericLiteral(node.value!));
+    case "NumberTypeAnnotation":
+      return tsNumberKeyword();
+    case "StringLiteralTypeAnnotation":
+      return tsLiteralType(stringLiteral(node.value!));
+    case "StringTypeAnnotation":
+      return tsStringKeyword();
+    case "ThisTypeAnnotation":
+      return tsThisType();
+    case "TupleTypeAnnotation":
+      return tsTupleType(node.types.map(toTsType));
+    case "TypeofTypeAnnotation":
+      return tsTypeQuery(getId(node.argument));
+
+    case "TypeAnnotation":
+      console.log(node);
+      return toTsType(node.typeAnnotation);
+
+    case "ObjectTypeAnnotation":
+      return tsTypeLiteral([
+        ...node.properties.map(_ => {
+          if (isSpreadProperty(_)) {
+            return _;
+          }
+          let s = tsPropertySignature(
+            _.key,
+            tsTypeAnnotation(toTsType(_.value))
+          );
+          s.optional = _.optional;
+          return s;
+          // TODO: anonymous indexers
+          // TODO: named indexers
+          // TODO: call properties
+          // TODO: variance
+        }),
+        // ...node.indexers.map(_ => tSIndexSignature())
+      ]);
+    case "UnionTypeAnnotation":
+      return tsUnionType(node.types.map(toTsType));
+    case "VoidTypeAnnotation":
+      return tsVoidKeyword();
+    case "QualifiedTypeAnnotation":
+      console.dir(node);
+    default:
+      throw new Error(`Didn't understand type '${node.type}'`);
   }
 }
 
 function getId(node: FlowType): Identifier {
   switch (node.type) {
-    case 'GenericTypeAnnotation': return node.id
-    default: throw ReferenceError('typeof query must reference a node that has an id')
+    case "GenericTypeAnnotation":
+      return node.id;
+    default:
+      throw ReferenceError("typeof query must reference a node that has an id");
   }
 }
 
 function functionToTsType(node: FunctionTypeAnnotation): TSFunctionType {
-
-  let typeParams = undefined
+  let typeParams = undefined;
 
   if (node.typeParameters) {
-    typeParams = tsTypeParameterDeclaration(node.typeParameters.params.map(_ => {
+    typeParams = tsTypeParameterDeclaration(
+      node.typeParameters.params.map(_ => {
+        // TODO: How is this possible?
+        if (isTSTypeParameter(_)) {
+          return _;
+        }
 
-      // TODO: How is this possible?
-      if (isTSTypeParameter(_)) {
-        return _
-      }
-
-      let constraint = _.bound ? toTs(_.bound) : undefined
-      let default_ = _.default ? toTs(_.default) : undefined
-      let param = tsTypeParameter(constraint, default_)
-      param.name = _.name
-      return param
-    }))
+        let constraint = _.bound ? toTs(_.bound) : undefined;
+        let default_ = _.default ? toTs(_.default) : undefined;
+        let param = tsTypeParameter(constraint, default_);
+        param.name = _.name;
+        return param;
+      })
+    );
   }
 
-  let f = tsFunctionType(typeParams)
+  let f = tsFunctionType(typeParams);
 
   // Params
   if (node.params) {
     // TODO: Rest params
-    let paramNames = node.params.map(_ => _.name).filter(_ => _ !== null).map(_ => (_ as Identifier).name)
+    let paramNames = node.params
+      .map(_ => _.name)
+      .filter(_ => _ !== null)
+      .map(_ => (_ as Identifier).name);
     f.parameters = node.params.map(_ => {
-      let name = _.name && _.name.name
+      let name = _.name && _.name.name;
 
       // Generate param name? (Required in TS, optional in Flow)
       if (name == null) {
-        name = generateFreeIdentifier(paramNames)
-        paramNames.push(name)
+        name = generateFreeIdentifier(paramNames);
+        paramNames.push(name);
       }
 
-      let id = identifier(name)
+      let id = identifier(name);
 
       if (_.typeAnnotation) {
-        id.typeAnnotation = tsTypeAnnotation(toTsType(_.typeAnnotation))
+        id.typeAnnotation = tsTypeAnnotation(toTsType(_.typeAnnotation));
       }
 
-      return id
-    })
+      return id;
+    });
   }
 
   // Return type
   if (node.returnType) {
-    f.typeAnnotation = tsTypeAnnotation(toTsType(node.returnType))
+    f.typeAnnotation = tsTypeAnnotation(toTsType(node.returnType));
   }
 
-  return f
+  return f;
 }
 
 function hasBound(node: Node): node is BoundedTypeParameter {
-  return isTypeParameter(node) && node.bound != null
+  return isTypeParameter(node) && node.bound != null;
 }
 
 interface BoundedTypeParameter extends TypeParameter {
-  bound: TypeAnnotation
+  bound: TypeAnnotation;
 }
--- src/index.ts
+++ src/index.ts
@@ -1,71 +1,88 @@
-import { parse } from '@babel/babylon'
-import generate from '@babel/generator'
-import traverse, { Node, Visitor } from '@babel/traverse'
-import { File } from '@babel/types'
-import { sync } from 'glob'
-import { dropWhile, pullAt } from 'lodash'
-import { EOL } from 'os'
-import { relative, resolve } from 'path'
+import { parse } from "@babel/parser";
+import generate from "@babel/generator";
+import traverse, { Node, Visitor } from "@babel/traverse";
+import { File } from "@babel/types";
+import { sync } from "glob";
+import { dropWhile, pullAt } from "lodash";
+import { EOL } from "os";
+import { relative, resolve } from "path";
 
-type Warning = [string, string, number, number]
-type Rule = (warnings: Warning[]) => Visitor<Node>
+type Warning = [string, string, number, number];
+type Rule = (warnings: Warning[]) => Visitor<Node>;
 
-let rules = new Map<string, Rule>()
+let rules = new Map<string, Rule>();
 
 export function addRule(ruleName: string, rule: Rule) {
   if (rules.has(ruleName)) {
-    throw `A rule with the name "${ruleName}" is already defined`
+    throw `A rule with the name "${ruleName}" is already defined`;
   }
-  rules.set(ruleName, rule)
+  rules.set(ruleName, rule);
 }
 
 export async function compile(code: string, filename: string) {
+  console.log("Parse...");
+  const parsed = parse(code, {
+    plugins: ["classProperties", "flow", "objectRestSpread"],
+    sourceType: "module",
+  });
+  console.log("Convert...");
+  let [warnings, ast] = await convert(parsed);
 
-  let [warnings, ast] = await convert(
-    parse(code, { plugins: ['classProperties', 'flow', 'objectRestSpread'], sourceType: 'module' })
-  )
-
+  console.log("Warnings...");
   warnings.forEach(([message, issueURL, line, column]) => {
-    console.log(`Warning: ${message} (at ${relative(__dirname, filename)}: line ${line}, column ${column}). See ${issueURL}`)
-  })
+    console.log(
+      `Warning: ${message} (at ${relative(
+        __dirname,
+        filename
+      )}: line ${line}, column ${column}). See ${issueURL}`
+    );
+  });
 
-  return addTrailingSpace(trimLeadingNewlines(generate(stripAtFlowAnnotation(ast)).code))
+  console.log("Strip flow annotations...");
+  return addTrailingSpace(
+    trimLeadingNewlines(generate(stripAtFlowAnnotation(ast)).code)
+  );
 }
 
 /**
  * @internal
  */
 export async function convert<T extends Node>(ast: T): Promise<[Warning[], T]> {
-
+  console.log("Loading rules...");
   // load rules directory
-  await Promise.all(sync(resolve(__dirname, './rules/*.js')).map(_ => import(_)))
+  await Promise.all(
+    sync(resolve(__dirname, "./rules/*.js")).map(_ => import(_))
+  );
 
-  let warnings: Warning[] = []
-  rules.forEach(visitor =>
-    traverse(ast, visitor(warnings))
-  )
+  let warnings: Warning[] = [];
+  rules.forEach((visitor, i) => {
+    console.log(`Running visitor ${i}`);
 
-  return [warnings, ast]
+    return traverse(ast, visitor(warnings));
+  });
+  console.log("Done");
+
+  return [warnings, ast];
 }
 
 function stripAtFlowAnnotation(ast: File): File {
-  let { leadingComments } = ast.program.body[0]
+  let { leadingComments } = ast.program.body[0];
   if (leadingComments) {
-    let index = leadingComments.findIndex(_ => _.value.trim() === '@flow')
+    let index = leadingComments.findIndex(_ => _.value.trim() === "@flow");
     if (index > -1) {
-      pullAt(leadingComments, index)
+      pullAt(leadingComments, index);
     }
   }
-  return ast
+  return ast;
 }
 
 function addTrailingSpace(file: string): string {
   if (file.endsWith(EOL)) {
-    return file
+    return file;
   }
-  return file + EOL
+  return file + EOL;
 }
 
 function trimLeadingNewlines(file: string): string {
-  return dropWhile(file.split(EOL), _ => !_).join(EOL)
+  return dropWhile(file.split(EOL), _ => !_).join(EOL);
 }
