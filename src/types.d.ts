3963ffeed25d0ce43536e34fd61b592e1cf3a283
Benjie Gillam <benjie@jemjie.com>
2019-03-12 23:17:34 +0000

    Better types

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,10 +1,3 @@
 declare module 'stdin'
 
 declare module '@babel/babylon'
-declare module '@babel/generator'
-declare module '@babel/traverse' {
-  let traverse: any
-  export default traverse
-  export type Node = any
-  export type Visitor<A> = any
-}
